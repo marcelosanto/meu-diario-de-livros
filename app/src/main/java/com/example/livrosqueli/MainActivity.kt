2883c1af77b15eee32d931371a79cdd9eb2fb4dd
Marcelo Santos <rocha.mer21@gmail.com>
2022-11-17 11:49:08 -0300

    feat: add hilt e configurado a injeção de depedencia

--- app/src/main/java/com/example/livrosqueli/MainActivity.kt
+++ app/src/main/java/com/example/livrosqueli/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.livrosqueli.ui.theme.LivrosQueLiTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/livrosqueli/ReaderApplication.kt
@@ -0,0 +1,7 @@
+package com.example.livrosqueli
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ReaderApplication : Application() {}
