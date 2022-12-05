ca7eab46672777d67a0567cf02a4eafb22889650
Marcelo Santos <rocha.mer21@gmail.com>
2022-12-05 18:51:07 -0300

    feat: UseForm, email and password input

--- app/src/main/java/com/example/livrosqueli/screens/login/ReaderBookLoginScreen.kt
+++ app/src/main/java/com/example/livrosqueli/screens/login/ReaderBookLoginScreen.kt
@@ -1,10 +1,122 @@
 package com.example.livrosqueli.screens.login
 
-import androidx.compose.material.Text
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.livrosqueli.R
+import com.example.livrosqueli.components.EmailInput
+import com.example.livrosqueli.components.PasswordInput
+import com.example.livrosqueli.components.ReaderLogo
 
 @Composable
 fun ReaderBookLoginScreen(navController: NavController) {
-    Text(text = "Login")
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            ReaderLogo()
+            UserForm(loading = false, isCreateAccount = true) { email, password ->
+                Log.d("TAG", "ReaderBookLoginScreen: $email - $password")
+
+            }
+        }
+    }
 }
+
+@Preview
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = { email, pwd -> }
+) {
+    val email = rememberSaveable { mutableStateOf("") }
+    val password = rememberSaveable { mutableStateOf("") }
+    val passwordVisibility = rememberSaveable { mutableStateOf(false) }
+    val passwordFocusRequest = FocusRequester.Default
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val valid = remember(email.value, password.value) {
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+    }
+
+    val modifier = Modifier
+        .height(250.dp)
+        .background(MaterialTheme.colors.background)
+        .verticalScroll(
+            rememberScrollState()
+        )
+
+    Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        if (isCreateAccount) Text(text = stringResource(id = R.string.create_acct))
+        else Text(text = "")
+        EmailInput(
+            emailState = email,
+            enabled = !loading,
+            onAction = KeyboardActions { passwordFocusRequest.requestFocus() })
+
+        PasswordInput(
+            modifier = Modifier.focusRequester(passwordFocusRequest),
+            passwordState = password,
+            labelId = "Password",
+            enabled = !loading,
+            passwordVisibility = passwordVisibility,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onDone(email.value.trim(), password.value.trim())
+            })
+
+        SubmitButton(
+            textId = if (isCreateAccount) "Sign upt" else "Login",
+            loading = loading,
+            validInputs = valid
+        ) {
+            onDone(email.value.trim(), password.value.trim())
+            keyboardController?.hide()
+        }
+
+    }
+}
+
+@Composable
+fun SubmitButton(
+    textId: String,
+    loading: Boolean,
+    validInputs: Boolean,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth(),
+        enabled = !loading && validInputs,
+        shape = CircleShape
+    ) {
+        if (loading) CircularProgressIndicator(modifier = Modifier.size(25.dp))
+        else Text(text = textId, modifier = Modifier.padding(5.dp))
+    }
+}
+
+
+
