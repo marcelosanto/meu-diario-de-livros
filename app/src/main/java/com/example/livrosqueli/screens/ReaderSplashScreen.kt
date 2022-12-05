e0323bb009dc962f3f904c2a39cb3bbdc16188b2
Marcelo Santos <rocha.mer21@gmail.com>
2022-12-05 15:41:57 -0300

    feat: ReaderSplashScreen

--- app/src/main/java/com/example/livrosqueli/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/example/livrosqueli/screens/ReaderSplashScreen.kt
@@ -1,10 +1,69 @@
 package com.example.livrosqueli.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.livrosqueli.navigation.ReaderScreens
+import kotlinx.coroutines.delay
 
 @Composable
 fun ReaderSplashScreen(navController: NavController) {
-    Text(text = "Splashscreen")
+
+    val scale = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(targetValue = 0.9f, animationSpec = tween(durationMillis = 800, easing = {
+            OvershootInterpolator(8f).getInterpolation(it)
+        }))
+
+        delay(2000L)
+        
+        navController.navigate(ReaderScreens.LoginScreen.name)
+    }
+
+    Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp)
+            .scale(scale.value),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                "A. Reader",
+                style = MaterialTheme.typography.h3,
+                color = Color.Red.copy(alpha = 0.5f)
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(
+                text = "\"Read. Change. Yourself \"",
+                style = MaterialTheme.typography.h5,
+                color = Color.LightGray
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/livrosqueli/screens/login/ReaderBookLoginScreen.kt
+++ app/src/main/java/com/example/livrosqueli/screens/login/ReaderBookLoginScreen.kt
@@ -1,8 +1,10 @@
 package com.example.livrosqueli.screens.login
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun ReaderBookLoginScreen(navController: NavController) {
+    Text(text = "Login")
 }
