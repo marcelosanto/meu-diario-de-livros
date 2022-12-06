bbe1471397eaf7075435cc28e7e6b9810dee0832
Marcelo Santos <rocha.mer21@gmail.com>
2022-12-06 13:50:47 -0300

    feat: createUser db

--- app/src/main/java/com/example/livrosqueli/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/example/livrosqueli/screens/login/LoginScreenViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 
@@ -44,6 +45,8 @@ class LoginScreenViewModel : ViewModel() {
 
             auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    val displayName = task.result?.user?.email?.split("@")?.get(0)
+                    createUser(displayName)
                     home()
                 } else {
                     Log.d("TAG", "createUserWithEmailAndPassword: ${task.result.toString()}")
@@ -51,6 +54,16 @@ class LoginScreenViewModel : ViewModel() {
                 _loading.value = false
             }
         }
+    }
+
+    private fun createUser(displayName: String?) {
+        val userId = auth.currentUser?.uid
+        val user = mutableMapOf<String, Any>()
+        user["user_id"] = userId.toString()
+        user["display_name"] = displayName.toString()
+
+        FirebaseFirestore.getInstance().collection("users")
+            .add(user)
     }
 
 }
