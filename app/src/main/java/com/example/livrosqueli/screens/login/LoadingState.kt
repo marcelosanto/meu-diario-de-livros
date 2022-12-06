68279ed21eeced7765bdd57962966c0447eb622f
Marcelo Santos <rocha.mer21@gmail.com>
2022-12-06 11:10:13 -0300

    feat: LoginState and enum of result

---
+++ app/src/main/java/com/example/livrosqueli/screens/login/LoadingState.kt
@@ -0,0 +1,18 @@
+package com.example.livrosqueli.screens.login
+
+data class LoadingState(val status: Status, val message: String? = null) {
+    companion object {
+        val IDLE = LoadingState(Status.IDLE)
+        val SUCCESS = LoadingState(Status.SUCCESS)
+        val FAILED = LoadingState(Status.FAILED)
+        val LOADING = LoadingState(Status.LOADING)
+    }
+
+    enum class Status {
+        SUCCESS,
+        FAILED,
+        LOADING,
+        IDLE;
+    }
+
+}
