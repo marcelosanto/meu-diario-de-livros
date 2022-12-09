96b3d41ded4afb988de6527b9ade884d95fdc3ed
Marcelo Santos <rocha.mer21@gmail.com>
2022-12-09 15:55:55 -0300

    feat: ReaderAppBar

--- app/src/main/java/com/example/livrosqueli/screens/home/ReaderBookHomeScreen.kt
+++ app/src/main/java/com/example/livrosqueli/screens/home/ReaderBookHomeScreen.kt
@@ -1,10 +1,85 @@
 package com.example.livrosqueli.screens.home
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
 fun ReaderBookHomeScreen(navController: NavController) {
-    Text(text = "HOME")
+    Scaffold(topBar = {
+        ReaderAppBar(title = "A.Reader", navController = navController)
+    }, floatingActionButton = {
+        FABContent {}
+    }) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            Text(text = "OIEEEE")
+        }
+    }
+}
+
+@Composable
+fun ReaderAppBar(
+    title: String,
+    showProfile: Boolean = true,
+    navController: NavController
+) {
+    TopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                if (showProfile) {
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = "Logo icon",
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(12.dp))
+                            .scale(0.1f)
+                    )
+                }
+                Text(
+                    text = title, color = Color.Red.copy(alpha = 0.7f),
+                    style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                )
+            }
+        },
+        actions = {},
+        backgroundColor = Color.Transparent,
+        elevation = 0.dp
+    )
+
+
+}
+
+@Composable
+fun FABContent(onTap: () -> Unit) {
+    FloatingActionButton(
+        onClick = { onTap() },
+        shape = RoundedCornerShape(50.dp),
+        backgroundColor = Color(0xFF92CBDF)
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "Add a book",
+            tint = Color.White
+        )
+    }
 }
