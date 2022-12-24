1c57e8c9749659824ce383e12d7577e6eb066971
Marcelo Santos <rocha.mer21@gmail.com>
2022-12-24 10:50:56 -0300

    feat: model MBook

---
+++ app/src/main/java/com/example/livrosqueli/model/MBook.kt
@@ -0,0 +1,8 @@
+package com.example.livrosqueli.model
+
+data class MBook(
+    var id: String? = null,
+    var title: String? = null,
+    var authors: String? = null,
+    var notes: String? = null,
+)
