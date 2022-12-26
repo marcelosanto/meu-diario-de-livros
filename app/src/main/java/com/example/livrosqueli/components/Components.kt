3757f87a5058f1d632e47d5d0e73b0a872b5cd08
Marcelo Santos <rocha.mer21@gmail.com>
2022-12-26 16:43:43 -0300

    feat: ReadingRightNowArea Components

--- app/src/main/java/com/example/livrosqueli/components/Components.kt
+++ app/src/main/java/com/example/livrosqueli/components/Components.kt
@@ -1,5 +1,6 @@
 package com.example.livrosqueli.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -10,6 +11,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material.icons.filled.StarBorder
+import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
@@ -17,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -25,8 +28,12 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.livrosqueli.model.MBook
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
@@ -202,6 +209,75 @@ fun FABContent(onTap: () -> Unit) {
 }
 
 @Composable
+fun ListCard(book: MBook, onPressDetails: (String) -> Unit) {
+    val resources = LocalContext.current.resources
+    val displayMetrics = resources.displayMetrics
+    val screenWidth = displayMetrics.widthPixels / displayMetrics.density
+    val spacing = 10.dp
+
+    Card(
+        shape = RoundedCornerShape(29.dp),
+        backgroundColor = Color.White,
+        elevation = 6.dp,
+        modifier = Modifier
+            .padding(16.dp)
+            .height(242.dp)
+            .width(202.dp)
+            .clickable { onPressDetails(book.title.toString()) }
+    ) {
+        Column(
+            modifier = Modifier.width(screenWidth.dp - (spacing * 2)),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Row(horizontalArrangement = Arrangement.Center) {
+                AsyncImage(
+                    model = "https://m.media-amazon.com/images/I/51RlAPJ0DzL._SX260_.jpg",
+                    contentDescription = "Book image",
+                    modifier = Modifier
+                        .height(140.dp)
+                        .width(100.dp)
+                        .padding(4.dp)
+                )
+
+                Spacer(modifier = Modifier.width(50.dp))
+
+                Column(
+                    modifier = Modifier.padding(top = 25.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Favorite,
+                        contentDescription = "Favorite icon",
+                        modifier = Modifier.padding(bottom = 1.dp)
+                    )
+                    BookRating(score = 3.5)
+                }
+            }
+
+            Text(
+                text = "Book Title",
+                modifier = Modifier.padding(4.dp),
+                fontWeight = FontWeight.Bold,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "Authors: All...",
+                modifier = Modifier.padding(4.dp), style = MaterialTheme.typography.caption
+            )
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            RoundedButton(label = "Reading", radius = 70)
+        }
+    }
+}
+
+@Composable
 fun BookRating(score: Double) {
     Surface(
         modifier = Modifier
@@ -221,5 +297,31 @@ fun BookRating(score: Double) {
             )
             Text(text = "3.5", style = MaterialTheme.typography.subtitle1)
         }
+    }
+}
+
+@Preview
+@Composable
+fun RoundedButton(label: String = "Reading", radius: Int = 29, onPress: () -> Unit = {}) {
+    Surface(
+        modifier = Modifier.clip(
+            RoundedCornerShape(
+                topStartPercent = radius,
+                bottomEndPercent = radius
+            )
+        ),
+        color = Color(0xFF92CBDF)
+    ) {
+        Column(
+            modifier = Modifier
+                .width(90.dp)
+                .heightIn(40.dp)
+                .clickable { onPress() },
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = label, style = TextStyle(color = Color.White, fontSize = 15.sp))
+        }
+
     }
 }
--- app/src/main/java/com/example/livrosqueli/screens/home/ReaderBookHomeScreen.kt
+++ app/src/main/java/com/example/livrosqueli/screens/home/ReaderBookHomeScreen.kt
@@ -2,26 +2,20 @@ package com.example.livrosqueli.screens.home
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import com.example.livrosqueli.components.BookRating
 import com.example.livrosqueli.components.FABContent
+import com.example.livrosqueli.components.ListCard
 import com.example.livrosqueli.components.ReaderAppBar
 import com.example.livrosqueli.components.TitleSection
 import com.example.livrosqueli.model.MBook
@@ -54,6 +48,31 @@ fun HomeContent(onNavigateToReaderStatsScreen: () -> Unit) {
         email.split("@")[0]
     } else "N/A"
 
+    val listOfBooks = listOf(
+        MBook(
+            id = "kasokoa",
+            title = "Android em Acao",
+            authors = "okaokas",
+            notes = "uhuhu"
+        ), MBook(
+            id = "kasoko2a",
+            title = "Android em Acao",
+            authors = "okaokas",
+            notes = "uhuhu"
+        ), MBook(
+            id = "kasoko3a",
+            title = "Android em Acao",
+            authors = "okaokas",
+            notes = "uhuhu"
+        ), MBook(
+            id = "kasoko4a",
+            title = "Android em Acao",
+            authors = "okaokas",
+            notes = "uhuhu"
+        )
+    )
+
+
     Column(Modifier.padding(2.dp), verticalArrangement = Arrangement.Top) {
         Row(Modifier.align(alignment = Alignment.Start)) {
             TitleSection(label = "Your reading \n " + " activity right now...")
@@ -81,109 +100,26 @@ fun HomeContent(onNavigateToReaderStatsScreen: () -> Unit) {
                 Divider()
             }
         }
-        ListCard(
-            book = MBook(
-                id = "kasokoa",
-                title = "Android em Acao",
-                authors = "okaokas",
-                notes = "uhuhu"
-            ), onPressDetails = {})
-    }
-}
 
-@Preview
-@Composable
-fun RoundedButton(label: String = "Reading", radius: Int = 29, onPress: () -> Unit = {}) {
-    Surface(
-        modifier = Modifier.clip(
-            RoundedCornerShape(
-                topStartPercent = radius,
-                bottomEndPercent = radius
-            )
-        ),
-        color = Color(0xFF92CBDF)
-    ) {
-        Column(
-            modifier = Modifier
-                .width(90.dp)
-                .heightIn(40.dp)
-                .clickable { onPress() },
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = label, style = TextStyle(color = Color.White, fontSize = 15.sp))
-        }
-
+        ReadingRightNowArea(books = listOfBooks, onNavigateToReaderStatsScreen)
     }
 }
 
 @Composable
-fun ListCard(book: MBook, onPressDetails: (String) -> Unit) {
-    val resources = LocalContext.current.resources
-    val displayMetrics = resources.displayMetrics
-    val screenWidth = displayMetrics.widthPixels / displayMetrics.density
-    val spacing = 10.dp
+fun ReadingRightNowArea(books: List<MBook>, onNavigateToReaderStatsScreen: () -> Unit) {
+    ListCard(books.first(), onPressDetails = {})
 
-    Card(
-        shape = RoundedCornerShape(29.dp),
-        backgroundColor = Color.White,
-        elevation = 6.dp,
-        modifier = Modifier
-            .padding(16.dp)
-            .height(242.dp)
-            .width(202.dp)
-            .clickable { onPressDetails(book.title.toString()) }
-    ) {
-        Column(
-            modifier = Modifier.width(screenWidth.dp - (spacing * 2)),
-            horizontalAlignment = Alignment.Start
-        ) {
-            Row(horizontalArrangement = Arrangement.Center) {
-                AsyncImage(
-                    model = "https://m.media-amazon.com/images/I/51RlAPJ0DzL._SX260_.jpg",
-                    contentDescription = "Book image",
-                    modifier = Modifier
-                        .height(140.dp)
-                        .width(100.dp)
-                        .padding(4.dp)
-                )
+    TitleSection(label = "Reading List")
 
-                Spacer(modifier = Modifier.width(50.dp))
-
-                Column(
-                    modifier = Modifier.padding(top = 25.dp),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Icon(
-                        imageVector = Icons.Rounded.Favorite,
-                        contentDescription = "Favorite icon",
-                        modifier = Modifier.padding(bottom = 1.dp)
-                    )
-                    BookRating(score = 3.5)
-                }
+    LazyRow {
+        items(books) { book ->
+            ListCard(book = book) {
+                onNavigateToReaderStatsScreen()
             }
 
-            Text(
-                text = "Book Title",
-                modifier = Modifier.padding(4.dp),
-                fontWeight = FontWeight.Bold,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                text = "Authors: All...",
-                modifier = Modifier.padding(4.dp), style = MaterialTheme.typography.caption
-            )
-        }
-
-        Row(
-            horizontalArrangement = Arrangement.End,
-            verticalAlignment = Alignment.Bottom
-        ) {
-            RoundedButton(label = "Reading", radius = 70)
         }
     }
 }
+
 
 
